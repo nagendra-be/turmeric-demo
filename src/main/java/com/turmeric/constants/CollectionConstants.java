847dd4e8056f5543996c8a7bba9023f1f65bd9ed
nagendra462 <67103687+nagendra462@users.noreply.github.com>
2023-01-25 09:39:41 +0530

    added new db for turmeric

--- src/main/java/com/turmeric/constants/CollectionConstants.java
+++ src/main/java/com/turmeric/constants/CollectionConstants.java
@@ -1,13 +1,13 @@
 package com.turmeric.constants;
 
 public class CollectionConstants {
-	public static final String ORDERS= "orders_turmeric";
-	public static final String ORDER_REQUESTS= "order_requests_turmeric";
-	public static final String CUSTOMERS= "customers_turmeric";
-	public static final String DELIVERIES= "deliveries_turmeric";
+	public static final String ORDERS= "orders";
+	public static final String ORDER_REQUESTS= "order_requests";
+	public static final String CUSTOMERS= "customers";
+	public static final String DELIVERIES= "deliveries";
 	public static final String UNIQUE_VALUES= "unique_values";
 	public static final String COUNTER= "counter";
 	public static final String ACTIVITY_LOG= "activity";
-	public static final String PAYMENTS= "payments_turmeric";
+	public static final String PAYMENTS= "payments";
 	public static final String AMOUNT_VALUES= "amount_values";
 }
